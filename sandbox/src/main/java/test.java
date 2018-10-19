e3218b636f7b5bcbec603edd60aa694799a0344a
anait <anaitiwe@mail.ru>
2018-10-19 12:13:35 +0300

    создан проект для IDEA

--- sandbox/src/main/java/test.java
+++ sandbox/src/main/java/test.java
@@ -1,7 +1,6 @@
 public class test {
 
 
-public static void main (String[] args){
-System.out.println("Hello, World!");
-}
+public static void main (String[] args) { System.out.println("Hello, World!");}
+
 }
