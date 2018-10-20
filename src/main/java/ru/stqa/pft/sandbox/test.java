6b445df397ce0a670c9723fbd9ef9e76c3f56245
anait <anaitiwe@mail.ru>
2018-10-20 22:08:43 +0300

    Добавлено несколько функций

--- src/main/java/ru/stqa/pft/sandbox/test.java
+++ src/main/java/ru/stqa/pft/sandbox/test.java
@@ -4,20 +4,24 @@ public class test {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello ("world");
+        hello ("user");
 
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2 );
-        System.out.println(2 - 2);
-
-        System.out.println(1 /2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println(2.0 / 2);
-
-        System.out.println("2" + "2");
-        System.out.println((2 + 2) * 2);
-        System.out.println("2 + 2 = " + (2 + 2));
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area( a, b));
     }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+    public static double area (double len){
+        return len * len;
+    }
+    public static double area(double a, double b){
+        return a * b;
+    }
+
 }
