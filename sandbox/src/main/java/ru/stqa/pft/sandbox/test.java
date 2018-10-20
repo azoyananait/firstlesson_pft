9f7c17c306b0b4673721a8ce9888ce38c2e99bfc
anait <anaitiwe@mail.ru>
2018-10-20 10:14:32 +0300

    Проект отвязан от репозитория Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.sandbox;
 public class test {
 
 
-public static void main (String[] args) { System.out.println("Hello, World!");}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
 }
