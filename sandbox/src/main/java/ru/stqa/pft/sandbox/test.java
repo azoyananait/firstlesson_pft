1eb26015b94b3bb86bf33c27a4fb9e5bfd4e9f57
anait <anaitiwe@mail.ru>
2018-10-20 10:37:03 +0300

    Изменения названия

--- sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/test.java
@@ -5,6 +5,19 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
 
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2 );
+        System.out.println(2 - 2);
+
+        System.out.println(1 /2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println((2 + 2) * 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+    }
 }
