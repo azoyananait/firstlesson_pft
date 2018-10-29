a15304b78ce8fcb0af8560ecfb4b000173bd00f7
anait <anaitiwe@mail.ru>
2018-10-29 13:31:02 +0300

    Создан тест для EditGroup(по уроку)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,4 +35,12 @@ public class GroupHelper extends HelperBase {
   public void returnToGroupPage() {
     click(By.linkText("group page"));
   }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 }
