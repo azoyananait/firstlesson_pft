46481c844978a01dbb5d53ea6890ce4b4a8c3858
anait <anaitiwe@mail.ru>
2018-11-11 16:00:04 +0300

    Лекция 4.1: Циклы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -10,13 +10,29 @@ public class Primes {
     return true;
   }
 
-  public static boolean isPrimeWhile(int n) {
-    int i = 2;
-    while (i < n) {
+  public static boolean isPrimeFast(int n) {
+    int m = (int) Math.sqrt(n);
+    for (int i = 2; i < m; i++) {
       if (n % i == 0) {
         return false;
       }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhile(int n) { // int -32 битные числа
+    int i = 2;
+    while (i < n && n % i != 0) {
       i++;
+    }
+    return i == n;
+  }
+
+  public static boolean isPrime(long n) { //long - 64 битные числа
+    for (long i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
     }
     return true;
   }
--- sandbox/src/test/Java/ru/stqa/pft/sandbox/PrimeTests.java
+++ sandbox/src/test/Java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -7,10 +7,15 @@ public class PrimeTests {
 
   @Test
   public void testPrimes() {
-    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
   }
   @Test
   public void testNonPrimes() {
     Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+  @Test (enabled = false)  // для отключения тестов
+  public void testPrimesLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
   }
 }
