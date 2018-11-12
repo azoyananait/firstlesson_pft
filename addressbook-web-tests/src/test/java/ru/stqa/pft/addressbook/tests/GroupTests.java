33993c85192b6c17686338dd3a69a083e5c241e0
anait <anaitiwe@mail.ru>
2018-11-12 17:38:41 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
@@ -4,11 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupTests extends TestBase {
   private final GroupData registrationData = new GroupData("test1", null, null);
-  private final GroupData modificationData = new GroupData("test1", "test2", "test3");
 
   @Test
   public void testGroupCreation() {
@@ -31,11 +31,16 @@ public class GroupTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().editGroup();
-    app.getGroupHelper().fillGroupForm(modificationData);
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().updateGroup();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
   @Test
