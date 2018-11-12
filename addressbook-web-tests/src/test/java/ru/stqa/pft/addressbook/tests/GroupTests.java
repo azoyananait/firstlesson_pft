ff0712ac7dec0218f49e8e4909bf33a300456041
anait <anaitiwe@mail.ru>
2018-11-12 15:01:16 +0300

    Лекция 4.6. Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
@@ -50,6 +50,9 @@ public class GroupTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+      Assert.assertEquals (before, after);
   }
 
 }
