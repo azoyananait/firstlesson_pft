743982553ea86806830414b9463d331f676686f5
anait <anaitiwe@mail.ru>
2018-11-29 18:43:03 +0300

    Лекция 6.6. Формат XML (по уроку для групп)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupTests.java
@@ -1,9 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,19 +28,38 @@ public class GroupTests extends TestBase {
     }
   }
 
-  @Test
-  public void testGroupCreation() {
-    app.group().cleanCache();
-    app.group().goToGroupPage();
-    Groups before = app.group().all();
-    app.group().create(registrationData);
-    assertEquals(app.group().count(), before.size() + 1);
-    Groups after = app.group().all();
-
-
-    assertThat(after, equalTo(
-            before.withAdded(registrationData.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+ //   List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+   //   String[] split = line.split(";");
+   //   list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>)xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  //  return list.iterator();
   }
+
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
+      app.group().cleanCache();
+      app.group().goToGroupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertEquals(app.group().count(), before.size() + 1);
+      Groups after = app.group().all();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
   @Test
   public void testBadGroupCreation() {
     app.group().cleanCache();
