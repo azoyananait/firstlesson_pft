97e109fc8d28c36dffa4365a8ee8f281383fef89
anait <anaitiwe@mail.ru>
2018-11-29 21:35:41 +0300

    Задание №14: Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,9 +7,15 @@ import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager{
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -19,10 +25,16 @@ public class ApplicationManager{
   private String browser;
 
   public ApplicationManager(String browser) {
+    properties = new Properties();
     this.browser = browser;
   }
 
-  public void init() {
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
     if(browser.equals(BrowserType.SAFARI)){
       wd = new SafariDriver();
     }else if(browser.equals(BrowserType.OPERA_BLINK)){
@@ -35,12 +47,11 @@ public class ApplicationManager{
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/index.php");
-
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     contactHelper = new ContactHelper(wd);
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -8,7 +8,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
