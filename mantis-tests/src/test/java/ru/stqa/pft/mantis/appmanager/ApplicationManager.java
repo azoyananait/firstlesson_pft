cb6c1e46ebe18d00b88c5c949a3e30e9e78e4a0d
anait <anaitiwe@mail.ru>
2018-12-09 21:01:32 +0300

    Добавлен новый модуль для тестирования MantisBT
    Подготовлен новый проект для нового учебного приложения:
    
    Создан новый модуль в IDEA
    Добавлен в него конфигурационный файл Gradle и всё необходимое для запуска сборщика
    Создана структура директорий для хранения кода.
    Создан TestBase и ApplicationManager (перенесен из существующего модуля и адаптировать, удалить весь неиспользуемый код).

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook.appmanager;
+package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,22 +8,18 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager{
+public class ApplicationManager {
   private final Properties properties;
   WebDriver wd;
 
-  private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private ContactHelper contactHelper;
+
   private String browser;
-  private DbHelper dbHelper;
+
 
   public ApplicationManager(String browser) {
     properties = new Properties();
@@ -35,7 +31,6 @@ public class ApplicationManager{
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    dbHelper = new DbHelper();
 
 
     if(browser.equals(BrowserType.SAFARI)){
@@ -51,28 +46,9 @@ public class ApplicationManager{
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-    contactHelper = new ContactHelper(wd);
+
 
   }
 
   public void stop () { wd.quit(); }
-
-  public GroupHelper group() {
-    return groupHelper;
   }
-
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
-  }
-
-  public ContactHelper contact() {
-    return contactHelper;
-  }
-
-  public DbHelper db() { return dbHelper; }
-  public NavigationHelper goTo() { return navigationHelper; }
-}
