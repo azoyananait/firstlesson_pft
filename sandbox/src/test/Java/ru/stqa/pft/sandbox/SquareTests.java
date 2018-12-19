fdc6bdb75075b4d65e55e2b5f4f3a5c8c4cd5165
anait <anaitiwe@mail.ru>
2018-12-20 01:44:59 +0300

    Испорчен тест для проверки Jenkins

--- sandbox/src/test/Java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/Java/ru/stqa/pft/sandbox/SquareTests.java
@@ -7,7 +7,7 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(5);
-    Assert.assertEquals(s.area(), 25);
+    Assert.assertEquals(s.area(), 20);
 
   }
 }
