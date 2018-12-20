8fb9ff7f9be67a1ac88b51951a3c58baed7ee667
anait <anaitiwe@mail.ru>
2018-12-20 21:38:35 +0300

    test safari

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -25,7 +25,7 @@ public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static ApplicationManager app =
-          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+          new ApplicationManager(System.getProperty("browser", BrowserType.SAFARI));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
